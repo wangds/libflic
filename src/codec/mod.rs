42fe0c50a8a1dd29461a06c7fc81bd0c7b25d4fd
David Wang <millimillenary@gmail.com>
2016-09-24 15:57:45 +1000

    Add GroupByEq iterator.
    This iterator groups the two input streams based on whether
    corresponding items are equal.  It returns whether they are equal and
    the length until that comparison changes value.
    
    We have also extended its behaviour in the following ways to aid its
    use in the compression routines:
    
    - it is often useful to add an initial "Same(0)" group, corresponding
    to the initial skip operation; and
    
    - it is also useful to ignore final Same operation, since there's no
    need to store it due to the op count byte.

--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -7,6 +7,8 @@ macro_rules! module {
     };
 }
 
+use std::iter::Zip;
+
 use ::{FlicError,FlicResult,RasterMut};
 
 module!(codec001);
@@ -17,6 +19,33 @@ module!(codec013);
 module!(codec014);
 module!(codec015);
 module!(codec016);
+
+/*--------------------------------------------------------------*/
+
+/// Result of a GroupByX operation: a grouping type, start index, and
+/// length.
+#[derive(Clone,Copy,Debug,Eq,PartialEq)]
+enum Group {
+    Same(usize, usize),
+    Diff(usize, usize),
+}
+
+/// An iterator that groups the two input streams based on whether
+/// corresponding items are equal.  It returns whether they are equal
+/// and the length until that comparison changes value.
+///
+/// This is suitable for compressing skip/memcpy type codecs,
+/// e.g. FLI_COLOR64, FLI_COLOR256.
+#[allow(dead_code)]
+struct GroupByEq<I: Iterator> where I::Item: PartialEq {
+    iter: Zip<I, I>,
+    peek: Option<bool>,
+    idx: usize,
+    prepend_same_run: bool,
+    ignore_final_same_run: bool,
+}
+
+/*--------------------------------------------------------------*/
 
 /// Returns true if the chunk type modifies the palette.
 pub fn chunk_modifies_palette(magic: u16)
@@ -40,4 +69,103 @@ pub fn decode_chunk(magic: u16, buf: &[u8], dst: &mut RasterMut)
     }
 
     Ok(())
+}
+
+/*--------------------------------------------------------------*/
+
+#[allow(dead_code)]
+impl<I: Iterator> GroupByEq<I>
+        where I::Item: PartialEq {
+    /// Create a new GroupByEq iterator.
+    fn new(old: I, new: I) -> Self {
+        GroupByEq {
+            iter: old.zip(new),
+            peek: None,
+            idx: 0,
+            prepend_same_run: false,
+            ignore_final_same_run: false,
+        }
+    }
+
+    /// If set, and if the two buffers start on a "Diff" sequence,
+    /// then a "Same" group of length 0 will be added at the start.
+    fn set_prepend_same_run(mut self) -> Self {
+        assert!(self.peek.is_none());
+        self.prepend_same_run = true;
+        self
+    }
+
+    /// If set, and if the two buffers end on a "Same" sequence,
+    /// then this final "same" type group will be ignored.
+    fn set_ignore_final_same_run(mut self) -> Self {
+        self.ignore_final_same_run = true;
+        self
+    }
+}
+
+impl<I: Iterator> Iterator for GroupByEq<I>
+        where I::Item: PartialEq {
+    type Item = Group;
+
+    /// Advances the iterator and returns the next value.
+    fn next(&mut self) -> Option<Group> {
+        let expected: bool;
+        let start = self.idx;
+        let mut n = 0;
+
+        if self.prepend_same_run {
+            self.prepend_same_run = false;
+            expected = true;
+        } else if let Some(x) = self.peek {
+            self.peek = None;
+            expected = x;
+            n = n + 1;
+        } else if let Some((a, b)) = self.iter.next() {
+            expected = a == b;
+            n = n + 1;
+        } else {
+            return None;
+        }
+
+        for x in self.iter.by_ref().map(|(a, b)| a == b) {
+            if x == expected {
+                n = n + 1;
+            } else {
+                self.peek = Some(x);
+                break;
+            }
+        }
+
+        self.idx = self.idx + n;
+        if expected {
+            if self.ignore_final_same_run && self.peek.is_none() {
+                return None;
+            }
+            return Some(Group::Same(start, n));
+        } else {
+            return Some(Group::Diff(start, n));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Group,GroupByEq};
+
+    #[test]
+    fn test_group_by_eq() {
+        let xs = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+        let ys = [ 0, 0, 3, 4, 5, 0, 7, 8, 9 ];
+        let expected = [
+            Group::Same(0, 0), // prepend
+            Group::Diff(0, 2), Group::Same(2, 3), Group::Diff(5, 1) ];
+
+        let gs: Vec<Group>
+            = GroupByEq::new(xs.iter(), ys.iter())
+            .set_prepend_same_run()
+            .set_ignore_final_same_run()
+            .collect();
+
+        assert_eq!(&gs[..], expected);
+    }
 }
